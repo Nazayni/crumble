5a776b2ad4e9787d4bf996575b0ce91c4a83d4b4
Nazayni <44854530+Nazayni@users.noreply.github.com>
2025-02-20 22:56:02 -0500

    Delete crumble directory

--- crumble/posts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . import models
-
-class CreatePost(forms.ModelForm):
-    class Meta:
-        model = models.Posts
-        fields = ['title', 'body', 'slug', 'banner']
--- crumble/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=40)
-    userpass = models.CharField(max_length=75)
-    email = models.CharField(max_length=75)
-    date = models.DateTimeField(auto_now_add=True)
