38f5d775dea48580ceee9a49610b2a49df3f5518
Nazayni <44854530+Nazayni@users.noreply.github.com>
2025-05-08 19:39:23 -0400

    Add files via upload

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,46 @@
 from django import forms
-from . import models
+from .models import Post # Make sure to import your Post model
 
-class CreatePost(forms.ModelForm):
+class PostUpdateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["banner"].widget.attrs.update({
+            'required': '',
+            'name': 'banner',
+            'id': 'banner',
+            'class': 'form-input',
+            'placeholder': 'Select banner image'
+        })
+        self.fields["title"].widget.attrs.update({
+            'required': '',
+            'name': 'title',
+            'id': 'title',
+            'type': 'text',
+            'class': 'form-input',
+            'placeholder': 'Post Title',
+            'maxlength': '100',
+            'minlength': '5'
+        })
+        self.fields["slug"].widget.attrs.update({
+            'required': '',
+            'name': 'slug',
+            'id': 'slug',
+            'type': 'text',
+            'class': 'form-input',
+            'placeholder': 'Post Slug',
+            'maxlength': '100',
+            'minlength': '5'
+        })
+        self.fields["body"].widget.attrs.update({
+            'required': '',
+            'name': 'body',
+            'id': 'body',
+            'class': 'form-input',
+            'placeholder': 'Post Body',
+            'rows': '5',
+            'cols': '50'
+        })
+
     class Meta:
-        model = models.Posts
-        fields = ['title', 'body', 'slug', 'banner']
+        model = Post
+        fields = ['banner', 'title', 'slug', 'body']
--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     #the fields relate to forms
     #for small amount of text
     title = models.CharField(max_length=75)
@@ -13,7 +13,7 @@ class Posts(models.Model):
     #adds current time stamp of the post
     date = models.DateTimeField(auto_now_add=True)
     banner = models.ImageField(default='fallback.png', blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
     def __str__(self):
         return self.title
