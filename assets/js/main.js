c7be00a3b4c5f0fed4c924cdd14d24215f96755b
Nazayni <44854530+Nazayni@users.noreply.github.com>
2025-05-08 19:26:46 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 console.log('This is JS from your About page.')
+
+function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const dropdown = document.querySelector('.dropdown');
+	const toggle = dropdown.querySelector('.dropdown-toggle');
+
+	toggle.addEventListener('click', () => {
+		dropdown.classList.toggle('open');
+	});
+});
